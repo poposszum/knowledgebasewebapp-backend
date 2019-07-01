990a9e3ceea331d6ecf13c9bf188ef7ad4aa73e5
David Papp1 <David_Papp1@epam.com>
2019-07-01 10:57:31 +0200

    deleted

--- src/main/java/com/company/knowledgebasebackend/article/ArticleEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.knowledgebasebackend.article;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleEntityRepository extends MongoRepository<ArticleEntity, ObjectId> {
-}
--- src/main/java/com/company/knowledgebasebackend/user/UserEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package com.company.knowledgebasebackend.user;
-
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserEntityRepository extends MongoRepository<UserEntity, ObjectId> {
-}
